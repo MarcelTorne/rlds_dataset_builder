c7037fc652beb0b3638bbd1ea78a7afb414e2f17
Marcel Torne Villasevil <marceltovi@gmail.com>
2024-08-07 18:03:32 +0200

    add dataset franka builder

---
+++ example_dataset_franka/create_example_data.py
@@ -0,0 +1,36 @@
+import numpy as np
+import tqdm
+import os
+
+N_TRAIN_EPISODES = 12
+N_VAL_EPISODES = 3
+
+DATA_PATH = "/data/pulkitag/misc/marcel/simassets/PolicyLearning/demos/cupntrashrealimgteleop/"
+TASK_DESCRIPTION = 'pick bowl and place in sink'
+def create_fake_episode(idx, path):
+    episode = []
+    actions = np.load(DATA_PATH+f"actions_0_{idx}.npy")
+    images = np.load(DATA_PATH+f"images_0_{idx}.npy")
+    states = np.load(DATA_PATH+f"states_0_{idx}.npy")
+    for step in range(len(actions)):
+        episode.append({
+            'image': images[step],
+            'state': states[step],
+            'action': actions[step],
+            'language_instruction': TASK_DESCRIPTION,
+        })
+    np.save(path, episode)
+
+
+# create fake episodes for train and validation
+print("Generating train examples...")
+os.makedirs('data/train', exist_ok=True)
+for i in tqdm.tqdm(range(N_TRAIN_EPISODES)):
+    create_fake_episode(i, f'data/train/episode_{i}.npy')
+
+print("Generating val examples...")
+os.makedirs('data/val', exist_ok=True)
+for i in tqdm.tqdm(range(N_VAL_EPISODES)):
+    create_fake_episode(i, f'data/val/episode_{i}.npy')
+
+print('Successfully created example data!')
