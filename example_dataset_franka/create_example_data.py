7374e4a2be1ce3faa861de6351bc3f706a4dd265
Marcel Torne Villasevil <marceltovi@gmail.com>
2024-08-07 18:12:47 +0200

    add dataset franka builder

--- example_dataset_franka/create_example_data.py
+++ example_dataset_franka/create_example_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tqdm
 import os
+import cv2
 
 N_TRAIN_EPISODES = 12
 N_VAL_EPISODES = 3
@@ -8,15 +9,13 @@ N_VAL_EPISODES = 3
 DATA_PATH = "/data/pulkitag/misc/marcel/simassets/PolicyLearning/demos/cupntrashrealimgteleop/"
 TASK_DESCRIPTION = 'pick bowl and place in sink'
 def create_fake_episode(idx, path):
-    import IPython
-    IPython.embed()
     episode = []
     actions = np.load(DATA_PATH+f"actions_0_{idx}.npy")
-    images = np.load(DATA_PATH+f"images_0_{idx}.npy")
+    images = np.load(DATA_PATH+f"images_0_{idx}.npz")["arr_0"][0,:,0]
     states = np.load(DATA_PATH+f"states_0_{idx}.npy")
     for step in range(len(actions)):
         episode.append({
-            'image': images[step],
+            'image': cv2.resize(images[step].transpose((1,2,0)), (256, 256), interpolation = cv2.INTER_LINEAR),
             'state': states[step],
             'action': actions[step],
             'language_instruction': TASK_DESCRIPTION,
