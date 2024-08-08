d65e30ea69f26994e53206504a5d2f3b8e93bd22
Marcel Torne Villasevil <marceltovi@gmail.com>
2024-08-08 09:10:08 +0200

    Add data and obj2sinkscript

---
+++ obj2sink/franka_dataset_builder.py
@@ -0,0 +1,112 @@
+from typing import Iterator, Tuple, Any
+
+import glob
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_hub as hub
+
+
+class Franka(tfds.core.GeneratorBasedBuilder):
+    """DatasetBuilder for example dataset."""
+
+    VERSION = tfds.core.Version('1.0.0')
+    RELEASE_NOTES = {
+      '1.0.0': 'Initial release.',
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        """Dataset metadata (homepage, citation,...)."""
+        return self.dataset_info_from_configs(
+            features=tfds.features.FeaturesDict({
+                'steps': tfds.features.Dataset({
+                    'observation': tfds.features.FeaturesDict({
+                        'image': tfds.features.Image(
+                            shape=(256, 256, 3),
+                            dtype=np.uint8,
+                            encoding_format='png',
+                            doc='Main camera RGB observation.',
+                        ),
+                        'state': tfds.features.Tensor(
+                            shape=(9,),
+                            dtype=np.float32,
+                            doc='Robot state, consists of [3 xyz 4 quat 2 gripper].',
+                        )
+                    }),
+                    'action': tfds.features.Tensor(
+                        shape=(14,),
+                        dtype=np.float32,
+                        doc='Robot action, consists discretized action space.',
+                    ),
+                    'is_first': tfds.features.Scalar(
+                        dtype=np.bool_,
+                        doc='True on first step of the episode.'
+                    ),
+                    'is_last': tfds.features.Scalar(
+                        dtype=np.bool_,
+                        doc='True on last step of the episode.'
+                    ),
+                    'is_terminal': tfds.features.Scalar(
+                        dtype=np.bool_,
+                        doc='True on last step of the episode if it is a terminal step, True for demos.'
+                    ),
+                    'language_instruction': tfds.features.Text(
+                        doc='Language Instruction.'
+                    ),
+                }),
+            }))
+
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        """Define data splits."""
+        return {
+            'train': self._generate_examples(path='data/train/episode_*.npy'),
+            'val': self._generate_examples(path='data/val/episode_*.npy'),
+        }
+
+    def _generate_examples(self, path) -> Iterator[Tuple[str, Any]]:
+        """Generator of examples for each split."""
+
+        def _parse_example(episode_path):
+            # load raw data --> this should change for your dataset
+            data = np.load(episode_path, allow_pickle=True)     # this is a list of dicts in our case
+
+            # assemble episode --> here we're assuming demos so we set reward to 1 at the end
+            episode = []
+            for i, step in enumerate(data):
+                episode.append({
+                    'observation': {
+                        'image': step['image'],
+                        'state': step['state'],
+                    },
+                    'action': step['action'],
+                    'is_first': i == 0,
+                    'is_last': i == (len(data) - 1),
+                    'is_terminal': i == (len(data) - 1),
+                    'language_instruction': step['language_instruction'],
+                })
+
+            # create output data sample
+            sample = {
+                'steps': episode,
+            }
+
+            # if you want to skip an example for whatever reason, simply return None
+            return episode_path, sample
+
+        # create list of all examples
+        episode_paths = glob.glob(path)
+
+        # for smallish datasets, use single-thread parsing
+        for sample in episode_paths:
+            yield _parse_example(sample)
+
+        # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
+        # beam = tfds.core.lazy_imports.apache_beam
+        # return (
+        #         beam.Create(episode_paths)
+        #         | beam.Map(_parse_example)
+        # )
+
